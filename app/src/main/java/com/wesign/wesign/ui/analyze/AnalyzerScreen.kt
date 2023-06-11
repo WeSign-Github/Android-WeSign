a08ae9aed76d0c67e2ec57efe447d2ce7707ad3e
Rd M Hafizh S <42491579+HafizhS@users.noreply.github.com>
2023-06-12 00:46:57 +0700

    Fix ImageAnalysis output resolution too high causing delay when predicting

--- app/src/main/java/com/wesign/wesign/ui/analyze/AnalyzerScreen.kt
+++ app/src/main/java/com/wesign/wesign/ui/analyze/AnalyzerScreen.kt
@@ -3,12 +3,15 @@ package com.wesign.wesign.ui.analyze
 import android.Manifest
 import android.graphics.Bitmap
 import android.util.Log
+import android.util.Size
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888
 import androidx.camera.core.resolutionselector.AspectRatioStrategy
 import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.core.resolutionselector.ResolutionSelector.HIGH_RESOLUTION_FLAG_OFF
+import androidx.camera.core.resolutionselector.ResolutionStrategy
+import androidx.camera.core.resolutionselector.ResolutionStrategy.FALLBACK_RULE_CLOSEST_HIGHER
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
@@ -46,6 +49,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
@@ -110,6 +114,8 @@ fun AnalyzerScreen(
 ) {
     var lensFacing by remember { mutableStateOf(CameraSelector.LENS_FACING_BACK) }
     var detection by remember { mutableStateOf<Detection?>(null) }
+    var lastestSize by remember { mutableStateOf<IntSize>(IntSize(0, 0)) }
+    var lastestInferenceTime by remember { mutableStateOf<Long>(0) }
 
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
 
@@ -131,7 +137,9 @@ fun AnalyzerScreen(
                     detection?.let {
                         Log.d("Analyzer", it.categories[0].label)
                         Text(it.categories[0].label)
-                        Text("${(it.categories[0].score*100).toInt()}")
+                        Text("${(it.categories[0].score * 100).toInt()}")
+                        Text("InferenceTime: ${lastestInferenceTime}")
+                        Text("Size: $lastestSize")
                     }
                 }
             }
@@ -146,8 +154,10 @@ fun AnalyzerScreen(
             CameraView(
                 modifier = Modifier.matchParentSize(),
                 cameraLens = lensFacing,
-                onAnalyze = { result ->
+                onAnalyze = { result, inferenceTime, size ->
                     detection = result
+                    lastestInferenceTime = inferenceTime
+                    lastestSize = size
                 },
             )
 
@@ -166,7 +176,7 @@ fun AnalyzerScreen(
 private fun CameraView(
     modifier: Modifier = Modifier,
     cameraLens: Int = CameraSelector.LENS_FACING_BACK,
-    onAnalyze: (Detection?) -> Unit = {},
+    onAnalyze: (Detection?, Long, IntSize) -> Unit = { _, _, _ -> },
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
@@ -199,17 +209,17 @@ private fun CameraView(
                     results?.let {
                         if (results.size <= 0) {
                             Log.d("Analyzer", "Size Detection 0")
-                            onAnalyze(null)
+                            onAnalyze(null, inferenceTime, IntSize(imageWidth, imageHeight))
                             return@let
                         }
 
                         for (detect in results) {
                             Log.d("Analyzer", "Subject")
-                            onAnalyze(detect)
+                            onAnalyze(detect, inferenceTime, IntSize(imageWidth, imageHeight))
                         }
                     } ?: run {
                         Log.d("Analyzer", "No Subject")
-                        onAnalyze(null)
+                        onAnalyze(null, inferenceTime, IntSize(imageWidth, imageHeight))
                     }
                 }
             }
@@ -245,7 +255,7 @@ private fun CameraView(
             val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
                 .setResolutionSelector(
                     ResolutionSelector.Builder()
-                        .setAspectRatioStrategy(AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY)
+                        .setResolutionStrategy(ResolutionStrategy(Size(640,480),FALLBACK_RULE_CLOSEST_HIGHER))
                         .setHighResolutionEnabledFlag(HIGH_RESOLUTION_FLAG_OFF)
                         .build()
                 )
